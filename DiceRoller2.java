faa298cf02473579eaa3764447b45e47de22bb83
franneh <146883412+franneh@users.noreply.github.com>
2024-02-15 08:07:31 -1000

    Update DiceRoller2.java

--- DiceRoller2.java
+++ DiceRoller2.java
@@ -11,27 +11,51 @@ public class Psuedocode {
 	//instantiate string
 	String str = myReader.nextLine();	
 	
+	Character delete = ' ';
+	Character plus = '+';
+	int a = countChar(str, delete);
+	int b = countChar(str, plus);
 	//make method to get numbers
-	getNums(str);
+	getNums(str, a);
 	
 	//close scanner
 	myReader.close();
 
-	getNums(str);
+
+	
+	//instantiate a new String phrase
+		String str2 = new String("I love carl");
+		
+	//instantiate a new String, which uses removeSpaces to remove the spaces from str
+		String str3 = new String(removeSpaces(str)); 
+		
+	//print str3
+		System.out.println(str3); 
+		
+
+
+	
 	
 	}
 
-	private static void getNums(String str) {
+	
+
+	private static void getNums(String str, int a) {
 		// TODO Auto-generated method stub
+		
+		for (int i = 0; i < a; i++)
+		{
+
 	//start at the beginning of number up to "d"
 	String left = str.substring(0,str.indexOf("d"));	
 	//would identify number after the "d" on the right side
 	String right = str.substring(str.indexOf("d") + 1, str.length());
 	//print out number with first number on left and second on right
-	System.out.print(left + " " + right);
+	System.out.println("Rolling Dice" + left + " " + right+ "times");
 	
 	//call method for 
 	turnInt(left,right);
+		}
 	}
 
 private static void turnInt(String left, String right) {
@@ -61,18 +85,56 @@ private static void rollDice(int a, int b) {
 		int adder = (int)((double)Math.random() * b + 1);
 		
 		//print roll
-		System.out.print(adder);
+		System.out.println(adder);
 		
 		//add current roll to running roll
 		hold += adder;
 		
+		
+		
 	}
-			
-	//print all the rolls
-	System.out.println(hold);
+	//print out total of rolls		
+	System.out.println("The total of your rolls is " + hold);
+	
 	
 }
 	
+private static String removeSpaces(String str)
+{
+	//declare int spaceIndex to be 0 
+	int spaceIndex = 0; 
+	
+	//instantiate a new String
+	String noSpaces = new String("");
+	
+	//for loop that declares which int i as 0 and increments till its not less than the str
+	for (int i = 0; i < str.length(); i++) 
+	//this makes the loop runs the proper amount of times
+	{
+		
+	//if statement to check if the character at the ith index of the String is a space
+	if (str.charAt(i) == ' ')
+		//this ensures each character of the String is checked
+		{
+//concatenate noSpaces to include the str value from the previous space's index to the current index
+noSpaces = noSpaces.concat(str.substring(spaceIndex + 1, i)); 
+			
+	 //reassigns spaceIndex as the current index
+	spaceIndex = i; //reassigns spaceIndex as the current index 
+		}
+	}
+	 //constructs final noSpaces value through concatenation
+	noSpaces = String.valueOf(str.charAt(0)).concat(noSpaces).concat(str.substring(spaceIndex + 1)); 
+	//includes 1st character of str and characters after the last space 
+	
+	 //return noSpaces
+	return noSpaces;
+
+
+	
+}
+
+
 
 
 	
