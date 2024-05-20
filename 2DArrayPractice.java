07d82711caf69e4785cdd0cca304ade05b6b83f9
franneh <146883412+franneh@users.noreply.github.com>
2024-05-20 13:48:31 -1000

    Create 2DArrayPractice.java

---
+++ 2DArrayPractice.java
@@ -0,0 +1,53 @@
+public class TwoDArrTrav {
+
+	public static void main(String[] args) {
+		
+		
+		String [][] name = {{"Alice", "Bob", "Stacy", "Kai", "John", "Carl", },
+				{"Charlie", "David", "Hammah", "Bob", "Duke", "Fanum"},
+				{"Ella", "Fiona", "Cris", "Dillon", "Keola", "Taryn"},
+				{"Kaitlyn", "Kam", "Kira", "Len", "Tyler", "Fran"},
+				{"Luke", "Micah", "Ian", "Taum", "Vaughn", "Jhase"},
+				{"Paige", "Emma", "Calissa", "Reese", "Maila", "Maile"},
+				{"Abby", "Nua", "Kennedy", "Lacey", "Mimi", "Rylee"}};
+			
+		for(int i = 0; i < name.length; i++)
+		{
+			System.out.println(name [i][0]);
+			
+		}
+		
+		for(int i = 0; i < name[0].length; i++)
+		{
+			System.out.println(name [0][i]);
+		}
+		
+		for (int i = 0; i < name.length; i++)
+		{
+			for (int j = 0; j < name[0].length; i++)
+			{
+				System.out.println(name[i] [j]);
+			}
+		}
+		
+		
+		
+		}
+	public static double[][] allOnesDoubleArray(double[][] arr)
+	{
+		double[][] copyArr = new double[arr.length][arr[0].length]; 
+		for (int i = 0; i < arr.length; i++) 
+		{
+			for (int j = 0; j < arr[0].length; j++) 
+			{
+				copyArr[i][j] = 1.0; 
+			}
+		}
+		return copyArr; 
+	}
+		
+		
+
+	}
+
+
